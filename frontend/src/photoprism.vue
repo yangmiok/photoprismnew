5b535abc539932addbe97327e7a20b3fcc5d88c6
Taylor <tzhu@evolveconsulting.com.hk>
2020-04-05 21:48:13 +0800

    1

--- frontend/src/photoprism.vue
+++ frontend/src/photoprism.vue
@@ -12,9 +12,9 @@
                 <router-view></router-view>
             </v-content>
         </v-app>
-<!--        <div style="text-align: center">-->
-<!--            <img src="./frontend/src/resources/weixinok.png" width="200px" height="200px">-->
-<!--        </div>-->
+        <div style="text-align: center">
+            <img :src="../../frontend/src/resources/weixinok.png" width="200px" height="200px">
+        </div>
         <p-photo-viewer></p-photo-viewer>
     </div>
 </template>
